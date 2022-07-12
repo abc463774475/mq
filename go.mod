e82cc1d8bdbf9a4539689f95240e035c5c737757
hexiaodong <463774475@qq.com>
2022-07-12 14:47:48 +0800

    feat: frist commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module git.intra.123u.com/rometa/romq
+
+go 1.18
+
+require github.com/abc463774475/my_tool v1.0.2
---
+++ nats_test.go
@@ -0,0 +1,12 @@
+package romq
+
+import (
+	nlog "github.com/abc463774475/my_tool/n_log"
+	"log"
+	"testing"
+)
+
+func TestNats(t *testing.T) {
+	nlog.Info("11111")
+	log.Printf("11111\n")
+}
